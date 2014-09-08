7557aeb987e26ea46ac42e74cd5686d744a447a5
Eugeniy <Eugene@MacBook-Pro-Zena.local>
2014-09-08 18:54:11 +0300

    Add code

--- MU110-Fibonacci.playground/section-1.swift
+++ MU110-Fibonacci.playground/section-1.swift
@@ -1,5 +1,20 @@
-// Playground - noun: a place where people can play
+// Playground - Home Task #1
 
-import UIKit
 
-var str = "Hello, playground"
+
+func fibonacci(n:Int) -> (n:Int, result:Int) {
+    if (n == 0) { return (n,0) }
+    else
+    {
+        if ((n == -1) || (n == 1)) { return (n,1)}
+        else
+        {
+            if (n > 0) { return (n,fibonacci(n - 1).n + fibonacci(n - 2).result) }
+            else { return (n,fibonacci(n + 2).n - fibonacci(n + 1).result) }
+        }
+    }
+}
+
+
+
+println(fibonacci(10))
